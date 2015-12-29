203b156abd0dbb95bd1cec7abb8e97dd4d32ef65
Hector Martin <marcan@marcan.st>
2015-12-29 23:15:56 +0900

    HACK: silence some spammy debug

--- drivers/gpu/drm/radeon/cik.c
+++ drivers/gpu/drm/radeon/cik.c
@@ -8600,7 +8600,7 @@ restart_ih:
 				if (atomic_read(&rdev->irq.pflip[0]))
 					radeon_crtc_handle_vblank(rdev, 0);
 				rdev->irq.stat_regs.cik.disp_int &= ~LB_D1_VBLANK_INTERRUPT;
-				DRM_DEBUG("IH: D1 vblank\n");
+				//DRM_DEBUG("IH: D1 vblank\n");
 
 				break;
 			case 1: /* D1 vline */
@@ -8608,7 +8608,7 @@ restart_ih:
 					DRM_DEBUG("IH: IH event w/o asserted irq bit?\n");
 
 				rdev->irq.stat_regs.cik.disp_int &= ~LB_D1_VLINE_INTERRUPT;
-				DRM_DEBUG("IH: D1 vline\n");
+				//DRM_DEBUG("IH: D1 vline\n");
 
 				break;
 			default:
@@ -8630,7 +8630,7 @@ restart_ih:
 				if (atomic_read(&rdev->irq.pflip[1]))
 					radeon_crtc_handle_vblank(rdev, 1);
 				rdev->irq.stat_regs.cik.disp_int_cont &= ~LB_D2_VBLANK_INTERRUPT;
-				DRM_DEBUG("IH: D2 vblank\n");
+				//DRM_DEBUG("IH: D2 vblank\n");
 
 				break;
 			case 1: /* D2 vline */
@@ -8638,7 +8638,7 @@ restart_ih:
 					DRM_DEBUG("IH: IH event w/o asserted irq bit?\n");
 
 				rdev->irq.stat_regs.cik.disp_int_cont &= ~LB_D2_VLINE_INTERRUPT;
-				DRM_DEBUG("IH: D2 vline\n");
+				//DRM_DEBUG("IH: D2 vline\n");
 
 				break;
 			default:
@@ -8660,7 +8660,7 @@ restart_ih:
 				if (atomic_read(&rdev->irq.pflip[2]))
 					radeon_crtc_handle_vblank(rdev, 2);
 				rdev->irq.stat_regs.cik.disp_int_cont2 &= ~LB_D3_VBLANK_INTERRUPT;
-				DRM_DEBUG("IH: D3 vblank\n");
+				//DRM_DEBUG("IH: D3 vblank\n");
 
 				break;
 			case 1: /* D3 vline */
@@ -8668,7 +8668,7 @@ restart_ih:
 					DRM_DEBUG("IH: IH event w/o asserted irq bit?\n");
 
 				rdev->irq.stat_regs.cik.disp_int_cont2 &= ~LB_D3_VLINE_INTERRUPT;
-				DRM_DEBUG("IH: D3 vline\n");
+				//DRM_DEBUG("IH: D3 vline\n");
 
 				break;
 			default:
@@ -8690,7 +8690,7 @@ restart_ih:
 				if (atomic_read(&rdev->irq.pflip[3]))
 					radeon_crtc_handle_vblank(rdev, 3);
 				rdev->irq.stat_regs.cik.disp_int_cont3 &= ~LB_D4_VBLANK_INTERRUPT;
-				DRM_DEBUG("IH: D4 vblank\n");
+				//DRM_DEBUG("IH: D4 vblank\n");
 
 				break;
 			case 1: /* D4 vline */
@@ -8698,7 +8698,7 @@ restart_ih:
 					DRM_DEBUG("IH: IH event w/o asserted irq bit?\n");
 
 				rdev->irq.stat_regs.cik.disp_int_cont3 &= ~LB_D4_VLINE_INTERRUPT;
-				DRM_DEBUG("IH: D4 vline\n");
+				//DRM_DEBUG("IH: D4 vline\n");
 
 				break;
 			default:
@@ -8720,7 +8720,7 @@ restart_ih:
 				if (atomic_read(&rdev->irq.pflip[4]))
 					radeon_crtc_handle_vblank(rdev, 4);
 				rdev->irq.stat_regs.cik.disp_int_cont4 &= ~LB_D5_VBLANK_INTERRUPT;
-				DRM_DEBUG("IH: D5 vblank\n");
+				//DRM_DEBUG("IH: D5 vblank\n");
 
 				break;
 			case 1: /* D5 vline */
@@ -8728,7 +8728,7 @@ restart_ih:
 					DRM_DEBUG("IH: IH event w/o asserted irq bit?\n");
 
 				rdev->irq.stat_regs.cik.disp_int_cont4 &= ~LB_D5_VLINE_INTERRUPT;
-				DRM_DEBUG("IH: D5 vline\n");
+				//DRM_DEBUG("IH: D5 vline\n");
 
 				break;
 			default:
@@ -8750,7 +8750,7 @@ restart_ih:
 				if (atomic_read(&rdev->irq.pflip[5]))
 					radeon_crtc_handle_vblank(rdev, 5);
 				rdev->irq.stat_regs.cik.disp_int_cont5 &= ~LB_D6_VBLANK_INTERRUPT;
-				DRM_DEBUG("IH: D6 vblank\n");
+				//DRM_DEBUG("IH: D6 vblank\n");
 
 				break;
 			case 1: /* D6 vline */
@@ -8758,7 +8758,7 @@ restart_ih:
 					DRM_DEBUG("IH: IH event w/o asserted irq bit?\n");
 
 				rdev->irq.stat_regs.cik.disp_int_cont5 &= ~LB_D6_VLINE_INTERRUPT;
-				DRM_DEBUG("IH: D6 vline\n");
+				//DRM_DEBUG("IH: D6 vline\n");
 
 				break;
 			default:
@@ -8772,7 +8772,7 @@ restart_ih:
 		case 14: /* D4 page flip */
 		case 16: /* D5 page flip */
 		case 18: /* D6 page flip */
-			DRM_DEBUG("IH: D%d flip\n", ((src_id - 8) >> 1) + 1);
+			//DRM_DEBUG("IH: D%d flip\n", ((src_id - 8) >> 1) + 1);
 			if (radeon_use_pflipirq > 0)
 				radeon_crtc_handle_flip(rdev, (src_id - 8) >> 1);
 			break;
@@ -9085,7 +9085,7 @@ restart_ih:
 			}
 			break;
 		default:
-			DRM_DEBUG("Unhandled interrupt: %d %d\n", src_id, src_data);
+			//DRM_DEBUG("Unhandled interrupt: %d %d\n", src_id, src_data);
 			break;
 		}
 
